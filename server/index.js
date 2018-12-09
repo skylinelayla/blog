0bcf8926e760c7a7a9b288b6e64f90da3cbe5b40
skykun <skylinelayla2012@gmail.com>
2018-12-10 01:42:32 +0800

    blog 样式重构，待完善

---
+++ server/index.js
@@ -0,0 +1,3 @@
+/**
+ * 只是为了把 Markdown 传到 server 上
+ */
--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@
  * @author shikun
  */
 import {markdown} from 'markdown';
+import $ from 'jquery';
+import Mustache from 'mustache';
+
+const axios = require('axios');
+let index = [
+    {
+        title: 'first commit',
+        date: '2018-12-09'
+    }
+];
 
 (function () {
+
     let article = document.getElementsByClassName('article')[0];
     article.innerHTML = markdown.toHTML('### hello world');
+
+    
 })();
